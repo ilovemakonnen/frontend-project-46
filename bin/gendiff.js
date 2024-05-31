0d035e45946d11e901cd761fdb3b9c0c42b8ea64
Pavel <pasmetkin@mail.ru>
2024-05-31 12:09:27 +0300

    SecondStep

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,6 @@ const program = new Command();
 program
  .name('gendiff')
  .description('Compares two configuration files and shows a difference.')
- .version('1.0.0');
+ .version('1.0.0')
+ .option('-f, --format [type]', 'output format');
  program.parse();
